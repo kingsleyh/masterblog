d3febe525be5028baaa4e677e41e612adc704f21
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2012-05-16 10:42:27 +0100

    Added initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masterthought::Application.config.secret_token = '4ee3491b0bed5d0a4399dba6d51b9c666556b3212e110ac3426ac141292b0b60107f2ecdddeb4c89ffd526729f28a58c6a90b2e0887951bd4585a13537b9ea01'
